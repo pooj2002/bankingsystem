682240c742999cbde9b75a9fc40580e5ce8d2954
1192812 <poojasukumar1409@gmail.com>
2023-09-07 21:43:49 +0530

    Added registration page,performed account creation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/register" element={<RegisterPage />} />
+          {/*Should add login component*/}
+          <Route path="/login" element={<div>Login Page</div>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
